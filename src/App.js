d4b8d4032ddb757767e06afa3dbe834339c4fc8c
Tatev Grigoryan <grigoryantatev1990@gmail.com>
2019-07-27 11:30:48 +0400

    test login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './styles/App.css';
+import Login from "./contents/header/login";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <h1> Welcome to Front-dev</h1>
+          <Login/>
       </header>
     </div>
   );
